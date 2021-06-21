cecfc8939657d3ca86c83afeb4b40da5f2f95f97
Alix Stirling <alix.stirling.as@gmail.com>
2021-06-21 14:44:57 +0200

    Appareil photo

---
+++ app/src/main/java/com/example/sauvezwilly/Fragment1.java
@@ -0,0 +1,114 @@
+package com.example.sauvezwilly;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class Fragment1 extends Fragment {
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1888;
+    Button PrendrePhoto;
+    Button ImportPhoto;
+    ImageView imageView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment1_layout, container, false);
+
+        PrendrePhoto = (Button) rootView.findViewById(R.id.PrendrePhoto);
+        ImportPhoto = (Button) rootView.findViewById(R.id.ImportPhoto);
+        imageView = (ImageView) rootView.findViewById(R.id.imageView);
+
+        private final View.OnClickListener mListener = new View.OnClickListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onClick(View view) {
+                switch(view.getId()){
+                    case R.id.PrendrePhoto:
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                        break;
+                    case R.id.ImportPhoto:
+                        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(),
+                                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                            startGallery();
+                        } else {
+                            ActivityCompat.requestPermissions(getActivity(),
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    2000);
+                        }
+                }
+
+            }
+        });
+
+        return rootView;
+
+    }
+
+    private void startGallery() {
+        Intent cameraIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        cameraIntent.setType("image/*");
+        if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, 1000);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                // convert byte array to Bitmap
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
+                        byteArray.length);
+
+                imageView.setImageBitmap(bitmap);
+
+            }
+        }
+        if(resultCode == Activity.RESULT_OK) {
+            if(requestCode == 1000){
+                Uri returnUri = data.getData();
+                Bitmap bitmapImage = null;
+                try {
+                    bitmapImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), returnUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(bitmapImage);
+            }
+        }
+    }
+}
